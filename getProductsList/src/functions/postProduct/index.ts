3cf176c0a6edbea7f9b27e09c6fc890d8f7e79a9
Konstantin Dedulya <eximal@gmail.com>
2021-09-09 10:24:35 +0300

    feat: add validate

--- getProductsList/src/functions/postProduct/index.ts
+++ getProductsList/src/functions/postProduct/index.ts
@@ -1,5 +1,24 @@
 import { handlerPath } from '@libs/handlerResolver';
 
+const schema = {
+  type: "object",
+  properties: {
+    title: {
+      type: 'string'
+    },
+    description: {
+      type: 'string'
+    },
+    price: {
+      type: 'number'
+    },
+    count: {
+      type: 'number'
+    },
+  },
+  required: ['title', 'description', 'price', 'count']
+}
+
 export default {
   handler: `${handlerPath(__dirname)}/handler.main`,
   events: [
@@ -8,6 +27,11 @@ export default {
         method: 'post',
         path: 'products/',
         cors: true,
+        request: {
+          schemas: {
+            'application/json': schema
+          }
+        }
       },
     },
   ],
